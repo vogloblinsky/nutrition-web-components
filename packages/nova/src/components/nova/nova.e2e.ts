ce7b42add581f620e60376870fafb2d11b9f3ad8
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2019-01-19 17:11:33 +0100

    feat(nova): first e2e test

--- packages/nova/src/components/nova/nova.e2e.ts
+++ packages/nova/src/components/nova/nova.e2e.ts
@@ -9,6 +9,13 @@ describe('nova', () => {
         expect(element).toHaveClass('hydrated');
 
         /* Test score container has one CSS class */
+        const elementScore = await page.evaluateHandle(
+            `document.querySelector('nova-badge').shadowRoot.querySelector('.score')`
+        );
+        const classNameElement = await elementScore.getProperty('className');
+        const classNames = await classNameElement.jsonValue();
+        const classNamesArray = classNames.split(' ');
+        expect(classNamesArray).toContain('one');
 
         /* If score > 4, test score container has one CSS class */
     });
