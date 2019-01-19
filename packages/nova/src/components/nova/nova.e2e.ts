18c0c6506fc33b4d824d4cc2f21a7eec89c6e564
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2019-01-19 17:48:44 +0100

    feat(nova): e2e tests

--- packages/nova/src/components/nova/nova.e2e.ts
+++ packages/nova/src/components/nova/nova.e2e.ts
@@ -1,5 +1,15 @@
 import { newE2EPage } from '@stencil/core/testing';
 
+const testCSSScoreClass = async (page, classString) => {
+    const elementScore = await page.evaluateHandle(
+        `document.querySelector('nova-badge').shadowRoot.querySelector('.score')`
+    );
+    const classNameElement = await elementScore.getProperty('className');
+    const classNames = await classNameElement.jsonValue();
+    const classNamesArray = classNames.split(' ');
+    return expect(classNamesArray).toContain(classString);
+};
+
 describe('nova', () => {
     it('renders', async () => {
         const page = await newE2EPage();
@@ -7,16 +17,38 @@ describe('nova', () => {
         await page.setContent('<nova-badge score="1"></nova-badge>');
         const element = await page.find('nova-badge');
         expect(element).toHaveClass('hydrated');
+    });
 
-        /* Test score container has one CSS class */
-        const elementScore = await page.evaluateHandle(
-            `document.querySelector('nova-badge').shadowRoot.querySelector('.score')`
-        );
-        const classNameElement = await elementScore.getProperty('className');
-        const classNames = await classNameElement.jsonValue();
-        const classNamesArray = classNames.split(' ');
-        expect(classNamesArray).toContain('one');
+    it('Score container has one CSS class', async () => {
+        const page = await newE2EPage();
 
+        await page.setContent('<nova-badge score="1"></nova-badge>');
+        await testCSSScoreClass(page, 'one');
+    });
+
+    it('If change score, test score container has new CSS class', async () => {
+        const page = await newE2EPage();
+
+        await page.setContent('<nova-badge score="1"></nova-badge>');
         /* If score > 4, test score container has one CSS class */
+        await page.$eval('nova-badge', (elm: any) => {
+            elm.score = '3';
+        });
+        await page.waitForChanges();
+
+        await testCSSScoreClass(page, 'three');
+    });
+
+    it('If change score > 4, test score container has one CSS class', async () => {
+        const page = await newE2EPage();
+
+        await page.setContent('<nova-badge score="1"></nova-badge>');
+        /* If score > 4, test score container has one CSS class */
+        await page.$eval('nova-badge', (elm: any) => {
+            elm.score = '5';
+        });
+        await page.waitForChanges();
+
+        await testCSSScoreClass(page, 'one');
     });
 });
