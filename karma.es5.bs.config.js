9e146e98b292d23517c39f9a1d877b9fdf5547ae
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2019-05-03 10:21:27 +0200

    fix(components): e2e karma browserstack testing

--- karma.es5.bs.config.js
+++ karma.es5.bs.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const merge = require('webpack-merge');
 const bsSettings = require('@open-wc/testing-karma-bs/bs-settings.js');
-const karmaEs5Config = require('./karma.es5.config.js');
 
 module.exports = config => {
   config.set(merge(bsSettings(config), {
