e5cbab327925280db1a18445a091683e941eb610
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2019-05-03 10:27:55 +0200

    fix(components): e2e karma browserstack testing

--- karma.es5.bs.config.js
+++ karma.es5.bs.config.js
@@ -1,9 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const merge = require('webpack-merge');
 const bsSettings = require('@open-wc/testing-karma-bs/bs-settings.js');
+const createBaseConfig = require('./karma.conf.js');
 
 module.exports = config => {
-  config.set(merge(bsSettings(config), {
+  config.set(merge(bsSettings(config), createBaseConfig(config), {
     browserStack: {
       project: 'nutrition web components',
     },
